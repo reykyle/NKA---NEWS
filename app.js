d823afa6a7519d78e5b82295a3309baf66ff3bc5
unknown <abimedkhalil@gmail.com>
2018-05-23 03:38:58 +0100

    test32

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ const main= document.querySelector('main');
 const sourceSelector = document.querySelector('#sourceSelector');
 const defaultSource = 'national-geographic';
 
-
-
 window.addEventListener('load', async e => {
 	updateNews();
 	await updateSources();
@@ -26,18 +24,35 @@ window.addEventListener('load', async e => {
 			console.log(`SW NON ENREGITRÉ ! `);
 		}
 
-		let deferredPrompt;
+		window.addEventListener('beforeinstallprompt', (e) => {
+			e.preventDefault();
+			deferredPrompt = e;
+			// Update UI notify the user they can add to home screen
+			btnAdd.style.display = 'block';
+		  });
 
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent Chrome 67 and earlier from automatically showing the prompt
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-});
+
 
 	}
 
 });
+
+btnAdd.addEventListener('click', (e) => {
+	// hide our user interface that shows our A2HS button
+	btnAdd.style.display = 'none';
+	// Show the prompt
+	deferredPrompt.prompt();
+	// Wait for the user to respond to the prompt
+	deferredPrompt.userChoice
+	  .then((choiceResult) => {
+		if (choiceResult.outcome === 'accepted') {
+		  console.log('User accepted the A2HS prompt');
+		} else {
+		  console.log('User dismissed the A2HS prompt');
+		}
+		deferredPrompt = null;
+	  });
+  });
 
 async function updateSources(){
 
@@ -72,6 +87,4 @@ function createArticle(article){
 			    </div>
 			  `;
 }
-
-
 
