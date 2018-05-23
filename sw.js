adf178f069abbaf612e8f84d169b28b46400d365
unknown <abimedkhalil@gmail.com>
2018-05-23 02:41:34 +0100

    test14

--- sw.js
+++ sw.js
@@ -21,12 +21,14 @@ wb.router.registerRoute(/.*\.(png|jpg|jpeg|gif)/,wb.strategies.cacheFirst({
 	cacheableResponse: {statuses: [0,200]}
 }));
 
+let deferredPrompt;
+
 window.addEventListener('beforeinstallprompt', (e) => {
-	// Prevent Chrome 67 and earlier from automatically showing the prompt
-	e.preventDefault();
-	// Stash the event so it can be triggered later.
-	deferredPrompt = e;
-  });
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+});
 
 /*
 // activer le manifest en horsligne 
