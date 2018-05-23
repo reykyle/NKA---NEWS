f05b458fe56ccb057d91c17b2509e462111afb20
unknown <abimedkhalil@gmail.com>
2018-05-23 01:14:08 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,64 @@
+const apiKey = '9d6f75d59a2049e79a885e0731ba4a64'; 
+const main= document.querySelector('main');
+const sourceSelector = document.querySelector('#sourceSelector');
+const defaultSource = 'le-monde';
+
+window.addEventListener('load', async e => {
+	updateNews();
+	await updateSources();
+	sourceSelector.value = defaultSource;
+
+	sourceSelector.addEventListener('change' , e => { 
+
+		updateNews(e.target.value);
+
+	 } )
+
+	if ('serviceWorker' in navigator ) {
+		try{
+			navigator.serviceWorker.register('sw.js');
+			console.log(`SW ENREGITRÉ ! `);
+		}
+
+		catch(error){
+			console.log(`SW NON ENREGITRÉ ! `);
+		}
+
+	}
+
+});
+
+async function updateSources(){
+
+	const res = await fetch(`https://newsapi.org/v2/sources?apiKey=${apiKey}`);
+	const json = await res.json();
+
+	sourceSelector.innerHTML = json.sources
+	.map( src =>`<option value="${src.id}" > ${src.name}  </option>`)
+	.join('\n');
+
+
+}
+
+async function updateNews(  source = defaultSource) {
+	const res = await fetch(`https://newsapi.org/v2/top-headlines?sources=${source}&sortBy=top&apiKey=${apiKey}`);
+	const json = await res.json();
+
+	main.innerHTML = json.articles.map(createArticle).join('\n');
+}
+
+
+
+function createArticle(article){
+
+			return `
+			    <div class="article">
+			      <a href="${article.url}">
+			        <h2>${article.title}</h2>
+			        <img src="${article.urlToImage}" alt="${article.title}">
+			        <p>${article.description}</p>
+			      </a>
+			    </div>
+			  `;
+}
+
---
+++ sw.js
@@ -0,0 +1,77 @@
+importScripts('./node_modules/workbox-sw/build/importScripts/workbox-sw.dev.v2.1.3.js');
+const staticAssets = [
+		
+	'./',
+	'./styles.css',
+	'./app.js',
+	'./fallback.json',
+	'./images/photo.jpg',
+	'index.html'
+
+
+];
+
+const wb = new WorkboxSW();
+wb.precache(staticAssets);
+
+wb.router.registerRoute('https://newsapi.org/(.*)',wb.strategies.networkFirst());
+wb.router.registerRoute(/.*\.(png|jpg|jpeg|gif)/,wb.strategies.cacheFirst({
+	cacheName: 'news-images',
+	cacheExpiration: {maxEntries: 20, maxAgeSeconds: 12*60*60},
+	cacheableResponse: {statuses: [0,200]}
+}));
+
+/*
+// activer le manifest en horsligne 
+self.addEventListener("fetch", function(event) {} );
+
+
+self.addEventListener('install', async event => {
+
+	const cache = await caches.open('news-static');
+	cache.addAll(staticAssets);
+
+} );
+
+self.addEventListener(`fetch`, event => {
+
+	const req = event.request;
+	const url = new URL(req.url);
+	
+	if (url.origin === location.origin ) {
+
+		event.respondWith(cacheFirst(req));
+	}
+
+	else {
+
+		event.respondWith(networkFirst(req));
+	}
+
+
+});
+
+async function cacheFirst(req){
+
+	const cachedResponse = await caches.match(req);
+	return cachedResponse || fetch(req);
+}
+
+async function networkFirst(req) {
+
+	const cache = await caches.open('news-dynamic');
+
+	try {
+
+		const res = await fetch(req);
+		cache.put(req, res.clone());
+		return res;
+	}
+
+	catch (error) {
+		const  cachedResponse = await cache.match(req);
+		return cachedResponse || await caches.match('./fallback.json');
+	}
+}
+
+*/
