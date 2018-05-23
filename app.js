c9f3ef4c86b9e5d8bba4dc46a600bf8e1496f51a
unknown <abimedkhalil@gmail.com>
2018-05-23 03:31:11 +0100

    test31

--- app.js
+++ app.js
@@ -26,9 +26,14 @@ window.addEventListener('load', async e => {
 			console.log(`SW NON ENREGITRÉ ! `);
 		}
 
-		window.addEventListener('beforeinstallprompt', event => {
-            event.prompt();
-        });
+		let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+});
 
 	}
 
