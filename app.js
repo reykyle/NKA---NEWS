fe06182d15d5a7e3dcc85b8c19a10ef93e28c2be
unknown <abimedkhalil@gmail.com>
2018-05-26 23:57:37 +0100

    test47

--- app.js
+++ app.js
@@ -74,11 +74,49 @@ function displayNotification(){
 }
 */
 
+
+var deferredPrompt;
+var enableNotificationsButtons = document.querySelectorAll('.enable-notifications');
+
+if (!window.Promise) {
+  window.Promise = Promise;
+}
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/sw.js')
+    .then(function () {
+      console.log('Service worker registered!');
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+}
+
+window.addEventListener('beforeinstallprompt', function(event) {
+  console.log('beforeinstallprompt fired');
+  event.preventDefault();
+  deferredPrompt = event;
+  return false;
+});
+
 function displayConfirmNotification() {
-  var options = {
-    body: 'You successfully subscribed to our Notification service!'
-  };
-  new Notification('Successfully subscribed!', options);
+  if ('serviceWorker' in navigator) {
+    var options = {
+      body: 'You successfully subscribed to our Notification service!',
+      icon: '/src/images/icons/app-icon-96x96.png',
+      image: '/src/images/sf-boat.jpg',
+      dir: 'ltr',
+      lang: 'en-US', // BCP 47,
+      vibrate: [100, 50, 200],
+      badge: '/src/images/icons/app-icon-96x96.png'
+    };
+
+    navigator.serviceWorker.ready
+      .then(function(swreg) {
+        swreg.showNotification('Successfully subscribed (from SW)!', options);
+      });
+  }
 }
 
 function askForNotificationPermission() {
