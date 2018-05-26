d74447baa2f16dc96e41a9fe6c8bc2bb716e21cc
unknown <abimedkhalil@gmail.com>
2018-05-27 00:40:55 +0100

    test52

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 Notification.requestPermission(function(status) {
     console.log('Notification permission status:', status);
+    function displayNotification(){
+
+        if (Notification.permission === 'granted') {
+            navigator.serviceWorker.getRegistration().then(function(reg) {
+    
+        
+                reg.showNotification('Hello world!');
+            });
+        }
+    
+    }
 });
 	
-function displayNotification(){
-
-	if (Notification.permission === 'granted') {
-		navigator.serviceWorker.getRegistration().then(function(reg) {
-
-	
-			reg.showNotification('Hello world!');
-		});
-	}
-
-}
