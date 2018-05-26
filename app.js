26911754431bf624dc7976c3f4691c75aced744c
unknown <abimedkhalil@gmail.com>
2018-05-26 23:30:53 +0100

    test42

--- app.js
+++ app.js
@@ -67,21 +67,6 @@ function createArticle(article){
 Notification.requestPermission(function(status) {
     console.log('Notification permission status:', status);
 });
-
-function showNotification() {
-	Notification.requestPermission(function(result) {
-	  if (result === 'granted') {
-		navigator.serviceWorker.ready.then(function(registration) {
-		  registration.showNotification('Vibration Sample', {
-			body: 'Buzz! Buzz!',
-			icon: '../images/touch/chrome-touch-icon-192x192.png',
-			vibrate: [200, 100, 200, 100, 200, 100, 200],
-			tag: 'vibration-sample'
-		  });
-		});
-	  }
-	});
-	}
 	
 function displayConfirmNotification(){
 new Notification('Bienvenue dans le site NKA NEWS');
