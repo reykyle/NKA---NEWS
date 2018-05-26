c9149a14f47a3b7786e07a97d866af5507d5f501
unknown <abimedkhalil@gmail.com>
2018-05-27 00:37:10 +0100

    test51

--- app.js
+++ app.js
@@ -63,20 +63,5 @@ function createArticle(article){
 			  `;
 }
 
-Notification.requestPermission(function(status) {
-    console.log('Notification permission status:', status);
-});
-	
-function displayNotification(){
 
-	if (Notification.permission == 'granted') {
-		navigator.serviceWorker.getRegistration().then(function(reg) {
-	
-			// TODO 2.4 - Add 'options' object to configure the notification
-	
-			reg.showNotification('Hello world!');
-		});
-	}
-
-}
 
