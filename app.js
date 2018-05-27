1e358442e56d89d48b9f2af56505dc624469b13b
unknown <abimedkhalil@gmail.com>
2018-05-27 01:27:03 +0100

    test60

--- app.js
+++ app.js
@@ -32,22 +32,8 @@ if (Notification.permission == 'granted') {
 
 */
 
-navigator.serviceWorker.register('sw.js');
 
-function showNotification() {
-  Notification.requestPermission(function(result) {
-    if (result === 'granted') {
-      navigator.serviceWorker.ready.then(function(registration) {
-        registration.showNotification('Vibration Sample', {
-          body: 'Buzz! Buzz!',
-          icon: 'images/icons/icon-192x192.png',
-          vibrate: [200, 100, 200, 100, 200, 100, 200],
-          tag: 'vibration-sample'
-        });
-      });
-    }
-  });
-}
+
 
 window.addEventListener('load', async e => {
 	updateNews();
@@ -110,4 +96,17 @@ function createArticle(article){
 }
 
 
-
+function showNotification() {
+  Notification.requestPermission(function(result) {
+    if (result === 'granted') {
+      navigator.serviceWorker.ready.then(function(registration) {
+        registration.showNotification('Vibration Sample', {
+          body: 'Buzz! Buzz!',
+          icon: 'images/icons/icon-192x192.png',
+          vibrate: [200, 100, 200, 100, 200, 100, 200],
+          tag: 'vibration-sample'
+        });
+      });
+    }
+  });
+}
