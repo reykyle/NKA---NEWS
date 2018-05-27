e0d831eb511adfe467b905e5f9641a840eb94beb
unknown <abimedkhalil@gmail.com>
2018-05-27 01:07:20 +0100

    test57

--- app.js
+++ app.js
@@ -10,9 +10,18 @@ Notification.requestPermission(function(status) {
 
 function displayNotification(){
 
-if (Notification.permission == 'granted') {
+if (Notification.permission === 'granted') {
 	navigator.serviceWorker.getRegistration().then(function(reg) {
 
+		var options = {
+			body: 'Here is a notification body!',
+			icon: 'images/icons/icon-72x72.png',
+			vibrate: [100, 50, 100],
+			data: {
+				dateOfArrival: Date.now(),
+				primaryKey: 1
+			}
+		};
 
 		reg.showNotification('Hello world!');
 	});
