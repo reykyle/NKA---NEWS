6f173eb3c76c65f3330ae9984a19ce19a976dc94
unknown <abimedkhalil@gmail.com>
2018-06-05 04:31:16 +0000

    test104

--- main.js
+++ main.js
@@ -1,11 +1,7 @@
-let installPromptEvent;
+let deferredPrompt;;
 
 window.addEventListener('beforeinstallprompt', (event) => {
-  // Prevent Chrome <= 67 from automatically showing the prompt
-  //event.preventDefault();
-  event.prompt();
-  // Stash the event so it can be triggered later.
-  installPromptEvent = event;
-  // Update the install UI to notify the user app can be installed
-  document.querySelector('#install-button').disabled = false;
+    e.preventDefault();
+    deferredPrompt = e;
+  btnAdd.style.display = 'block';
 });
