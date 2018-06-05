1b2f50716703bc9f685e5ee7b0079cfde0451ea6
unknown <abimedkhalil@gmail.com>
2018-06-05 04:15:11 +0000

    test102

--- app.js
+++ app.js
@@ -64,16 +64,6 @@ function createArticle(article){
 			  `;
 }
 
-let installPromptEvent;
-
-window.addEventListener('beforeinstallprompt', (event) => {
-  // Prevent Chrome <= 67 from automatically showing the prompt
-  event.preventDefault();
-  // Stash the event so it can be triggered later.
-  installPromptEvent = event;
-  // Update the install UI to notify the user app can be installed
-  document.querySelector('#install-button').disabled = false;
-});
 
 /*
 Notification.requestPermission(function(status) {
